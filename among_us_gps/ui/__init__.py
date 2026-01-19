09a2f5a13f15af403b7738e923c9d2ef3fa8e5f8
Thomas Mercandino <thomas.mercandino14@gmail.com>
2026-01-19 17:59:53 +0000

    feat: inizializza il package ui

---
+++ among_us_gps/ui/__init__.py
@@ -0,0 +1,6 @@
+"""Componenti UI DearPyGui: dashboard principale ed editor azioni task."""
+
+from .gps_app import GPSVisualizerPro
+from .task_action_editor import TaskActionEditor
+
+__all__ = ["GPSVisualizerPro", "TaskActionEditor"]
